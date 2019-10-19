108e8920fe67d2d4b8ec24efd379f870bdd66481
romfrolov <justcallmefrolov@gmail.com>
2019-10-20 00:35:15 +0300

    Transactions API.

---
+++ api/transactions.js
@@ -0,0 +1,47 @@
+/**
+ * Transactions API.
+ *
+ * @module api/transactions
+ */
+
+'use strict';
+
+module.exports = Transactions;
+
+function Transactions(parent) {
+    if (!new.target) {
+        return new Transactions(parent);
+    }
+
+    Object.defineProperty(this, 'http', {value: parent.http});
+}
+
+Transactions.prototype.authCapture = function authCapture(cardTransaction) {
+    const path = '/services/2/transactions';
+
+    return this.http.post(path, cardTransaction);
+};
+
+Transactions.prototype.authOnly = function authOnly(cardTransaction) {
+    const path = '/services/2/transactions';
+
+    return this.http.post(path, cardTransaction);
+};
+
+Transactions.prototype.capture = function capture(cardTransaction) {
+    const path = '/services/2/transactions';
+
+    return this.http.put(path, cardTransaction);
+};
+
+Transactions.prototype.authReversal = function authReversal(cardTransaction) {
+    const path = '/services/2/transactions';
+
+    return this.http.put(path, cardTransaction);
+};
+
+Transactions.prototype.get = function get(transactionId) {
+    const path = `/services/2/transactions/${transactionId}`;
+
+    return this.http.get(path);
+};
